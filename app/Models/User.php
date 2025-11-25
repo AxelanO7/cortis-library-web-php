7fe4a5bfd2f4e496bed8d2ace1669a86a5b818ff
AxelanO7 <jeremia123.jm@gmail.com>
2025-11-26 00:43:24 +0800

    ♻️ refactor(user): migrate user model to eloquent model
    - change user model from migration file to eloquent model
    - add fillable, hidden, and casts properties for mass assignment, security, and type casting

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,21 +1,29 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+namespace App\Models;
 
-return new class extends Migration {
-    public function up(): void {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamps();
-        });
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-    public function down(): void {
-        Schema::dropIfExists('users');
-    }
-};
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
